8099271633c3d703caa63920c5e03b2d6eaff2b6
Cbermont <89049830+bermont13@users.noreply.github.com>
2025-02-20 03:30:20 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+  let simbolos = [ "♦", "♥", "♠", "♣"]
+  
+  let numero = numeros[Math.floor(Math.random() * numeros.length)]
+  let simbolo = simbolos[Math.floor(Math.random() * simbolos.length)]
+  
+  let simboloArriba = document.querySelector(".carta-arriba")
+  simboloArriba.innerText = simbolo
+  let simboloAbajo = document.querySelector(".carta-abajo")
+  simboloAbajo.innerText = simbolo
+
+  let simboloValor = document.querySelector(".valor")
+  simboloValor.innerText = numero
+  if (simbolo == "♦" || simbolo == "♥") {
+    simboloArriba.classList.add("texto-rojo")
+    simboloAbajo.classList.add("texto-rojo")
+  }
+
 };
+
